767cbb8c4c9ef4fca97e5d62aba677cc416ba058
wneto <n88131251>
2022-05-28 08:46:15 -0300

    Configuração da SECRET_KEY

--- nkpro/settings.py
+++ nkpro/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s7$x=4(7egi+=nfex6nn)3#)7td)*6@r@0ir!ekj$*$b=(&54!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
