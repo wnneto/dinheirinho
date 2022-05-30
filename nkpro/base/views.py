fd55d93cab2244fa61db1befada7a05ff135f19f
wneto <n88131251>
2022-05-30 05:33:02 -0300

    Config django_debug_toolbar

--- nkpro/base/views.py
+++ nkpro/base/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Nas Nuvens!!!!')
+    return HttpResponse('<html><body>NAS NUVENS!!!</body></html>', content_type='text/html')
