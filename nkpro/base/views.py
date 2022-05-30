cbb2bdb04fa293653ba4c62e7d71bbeb7e9451e8
wneto <n88131251>
2022-05-30 07:56:58 -0300

    Config django_debug_toolbar

--- nkpro/base/views.py
+++ nkpro/base/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('<html><body>NAS NUVENS!!!</body></html>', content_type='text/html')
+    return render(request, 'base/home.html')
