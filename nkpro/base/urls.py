9cfb0ada38b3356903ff3aabd9a37e875f3cbf3f
wneto <n88131251>
2022-06-01 02:13:41 -0300

    Config Landing Page

--- nkpro/base/urls.py
+++ nkpro/base/urls.py
@@ -14,19 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.conf import settings
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from nkpro.base.views import home
 
 
+app_name = 'base'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('nkpro.base.urls')),
+    path('', home, name='home'),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns.append(
-        path('__debug__/', include(debug_toolbar.urls))
-    )
